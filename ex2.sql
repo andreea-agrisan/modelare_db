f26275ec27671e9ed06abafba90f21aa6471c424
Andreea Agrisan <andreea.agrisan02@e-uvt.ro>
2023-12-16 12:10:08 +0200

    ex2

--- ex2.sql
+++ ex2.sql
@@ -1,9 +1,28 @@
+CREATE TABLE IF NOT EXISTS primarie(
+	id serial not null,
+	angajati integer not null,
+	primary key(id)
+);
+CREATE TABLE IF NOT EXISTS uat(
+	id serial not null,
+	primarie_id integer not null unique,
+	nume varchar(50) not null,
+	primary key(id),
+	foreign key(primarie_id) references primarie(id)
+);
 CREATE TABLE IF NOT EXISTS proprietar(
 	cnp integer NOT NULL,
 	serie_numar varchar(9) not null,
 	nume varchar(100) not null,
 	sex char(1) not null,
 	primary key(cnp,serie_numar)
+);
+CREATE TABLE IF NOT EXISTS teren(
+	id serial not null,
+	uat_id integer not null,
+	suprafata integer not null,
+	primary key(id),
+	foreign key(uat_id) references uat(id)
 );
 CREATE TABLE IF NOT EXISTS adrese(
 	id serial not null,
@@ -14,27 +33,17 @@ CREATE TABLE IF NOT EXISTS adrese(
 	primary key(id),
 	foreign key(proprietar_cnp,proprietar_serie_numar) references proprietar(cnp,serie_numar)
 );
-CREATE TABLE IF NOT EXISTS primarie(
-	id serial not null,
-	angajati integer not null,
-	primary key(id)
-);
-CREATE TABLE IF NOT EXISTS uat(
-	id serial not null,
-	primarie_id integer not null,
-	nume varchar(50) not null,
-	primary key(id),
-	foreign key(primarie_id) references primarie(id)
-);
-CREATE TABLE IF NOT EXISTS teren(
-	id serial not null,
-	uat_id integer not null,
-	suprafata integer not null,
-	primary key(id),
-	foreign key(uat_id) references uat(id)
-);
 CREATE TABLE IF NOT EXISTS registru_agricol(
 	teren_id integer not null,
-	primarie_id integer not null,
-	primary key(teren_id,primarie_id)
+	primarie_id integer not null unique,
+	primary key(teren_id,primarie_id),
+	foreign key(teren_id) references teren(id),
+	foreign key(primarie_id) references primarie(id)
+);
+CREATE TABLE IF NOT EXISTS proprietar_teren(
+	proprietar_cnp integer NOT NULL,
+	proprietar_serie_numar varchar(9) not null,
+	teren_id integer not null,
+	foreign key(proprietar_cnp,proprietar_serie_numar) references proprietar(cnp,serie_numar),
+	foreign key(teren_id) references teren(id)
 );
